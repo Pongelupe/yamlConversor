32ac7284018109e544f9fe35d711cbf1425d74f7
MrFancyPants <paulo.ribeiro.dev@gmail.com>
2017-09-14 15:04:34 -0300

    Meus equipamentos!

--- src/com/yamlConversor/compiler/CompileFromInput.java
+++ src/com/yamlConversor/compiler/CompileFromInput.java
@@ -5,8 +5,14 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,11 +29,25 @@ public class CompileFromInput {
 	private final String packageClasses = "com.yamlConversor.classes";
 	private File file;
 
-	public CompileFromInput(File file) {
-		this.file = file;
+	public CompileFromInput(File file) throws Exception {
+//        CopyOption[] options = new CopyOption[]{
+//                StandardCopyOption.REPLACE_EXISTING,
+//                StandardCopyOption.COPY_ATTRIBUTES
+//        };
+//        
+//        Path in = Paths.get(file.toURI());
+        Path out = Paths.get("C:\\projects\\java\\yamlConversor\\src\\com\\yamlConversor\\classes\\" + file.getName());
+        
+//		Files.copy(in, out, options);
+		
+		this.file = out.toFile();
+		
+		setPackage();
 	}
 
 	public void compile() throws Exception {
+		Path dest = Paths.get("C:\\projects\\java\\yamlConversor\\bin");
+		
 		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
 
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
@@ -36,7 +56,9 @@ public class CompileFromInput {
 		List<String> optionList = new ArrayList<String>();
 		optionList.add("-classpath");
 		optionList.add(System.getProperty("java.class.path") + ";dist/InlineCompiler.jar");
-
+		optionList.add("-d");
+		optionList.add(dest.toString());
+		
 		Iterable<? extends JavaFileObject> compilationUnit = fileManager
 				.getJavaFileObjectsFromFiles(Arrays.asList(file));
 		JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, optionList, null,
@@ -47,20 +69,16 @@ public class CompileFromInput {
 			 * Load and execute
 			 *************************************************************************************************/
 			System.out.println("Yipe");
-			System.out.println(getPackage());
-			setPackage();
-			System.out.println(getPackage());
 			// Create a new custom class loader, pointing to the directory that
 			// contains the compiled
 			// classes, this should point to the top of the package structure!
-			URLClassLoader classLoader = new URLClassLoader(
-					new URL[] { new File(System.getProperty("user.dir")).toURI().toURL() });
+			URLClassLoader classLoader = new URLClassLoader(new URL[] { new File("./").toURI().toURL() });
 			// Load the class from the classloader by name....
 			Class<?> loadedClass = classLoader.loadClass(getPackage() + file.getName().replace(".java", ""));
 			// Create a new instance...
 			Object obj = loadedClass.newInstance();
 			// Santity check
-			System.out.println(obj);
+			System.out.println(obj.getClass());
 			classLoader.close();
 
 			/*************************************************************************************************
@@ -100,8 +118,7 @@ public class CompileFromInput {
 		bw.flush();
 		bw.close();
 
-		file.createNewFile();
-		file.renameTo(temp);
+		Files.move(temp.toPath(), file.toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING); 
 
 	}
 
